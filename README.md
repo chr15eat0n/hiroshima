d698a8994b07a1eab5d799ec167d78c6df004e23
chr15eat0n <chris@chriseaton.net>
2015-07-30 02:40:33 -0300

    basic website

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # hiroshima
+
+Here's a project to learn some jquery as well as grow support for disarmement.
+
+Features I'd like to add as I have time:
+
+Use ajax to put data in spreadsheat and update that as arsenals increase and decrease.
+Make years since hiroshima go up every year (should be easy!)
--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 
+$(window).load(function() {
+		// Animate loader off screen
+		$("#intro").removeClass('hiddenDIV');
+    $("#outro").removeClass('hiddenDIV');
+    $(".loading").fadeOut("slow");;
+	});
+
+$(document).ready(function(){
+  $("#allBombs").ready(function(){
+    for (i=0; i<15695;i++) {
+        $("#allBombs").append("<img src='imgs/bomb.png' width='100px'>");
+    }
+  });
+
+});
